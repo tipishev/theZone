752e1d3bde6f008b473670eb78d4352d2ee78aef
Timofey Tipishev <tipishev@yandex-team.ru>
2015-09-26 18:30:59 +0500

    make viewport scrolling work

--- src/controls.c
+++ src/controls.c
@@ -3,30 +3,43 @@
 #include "graphics.h" // leaky abstraction for demo purposes x 2
 #include "model.h" // leaky abstraction for demo purposes x 2
 
+static  GPoint position;
+char world_piece[VIEWPORT_HEIGHT][VIEWPORT_WIDTH] = { // FIXME declare as static?
+    {'.', '.', '.', '.', '.'},
+    {'.', '.', '.', '.', '.'},
+    {'.', '.', '.', '.', '.'},
+    {'.', '.', '.', '.', '.'},
+    {'.', '.', '.', '.', '.'},
+    {'.', '.', '.', '.', '.'}
+  };
+
 static void increment_click_handler(ClickRecognizerRef recognizer, void *context) {
-  static char world_piece[VIEWPORT_HEIGHT][VIEWPORT_WIDTH];
-  give_world_piece(world_piece);
-  fill_viewport(world_piece);
+  if (position.y > 0) {
+    --position.y;
+    give_world_piece(position, world_piece);
+    fill_viewport(world_piece);
+  }
 }
 
 static void decrement_click_handler(ClickRecognizerRef recognizer, void *context) {
-  static char world_piece[VIEWPORT_HEIGHT][VIEWPORT_WIDTH] = {
-    {'#','#','#','.','.'},
-    {'#','@','#','.','Z'},
-    {'.','.','#','#','.'},
-    {'.','.','.','#','.'},
-    {'.','.','.','Z','.'},
-    {'Z','.','.','.','.'}
-  };
-  fill_viewport(world_piece);
+  if (position.y < WORLD_HEIGHT - 1) {
+    ++position.y;
+    give_world_piece(position, world_piece);
+    fill_viewport(world_piece);
+  }
 }
 
 static void run_click_handler(ClickRecognizerRef recognizer, void *context) {
-    vibe_footsteps();
+  if (position.x < WORLD_WIDTH - 1) {
+    ++position.x;
+    give_world_piece(position, world_piece);
+    fill_viewport(world_piece);
+  }
+  else position.x = 0;
 }
 
 static void switch_mode_click_handler(ClickRecognizerRef recognizer, void *context) {
-    vibe_explosion();
+    /*vibe_explosion();*/
     switch_action_bar();
 }
 
